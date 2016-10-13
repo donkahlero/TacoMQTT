7a79a8614766366cb19d5930afc447d39b4c8c07
TacoVox <jonas.kahler@icloud.com>
2016-10-13 19:41:50 +0200

    Letzte Änderungen

--- apps/tacomqtt/src/tacomqtt_app.erl
+++ apps/tacomqtt/src/tacomqtt_app.erl
@@ -19,11 +19,11 @@ start(_StartType, _StartArgs) ->
     %%ok = application:start(ranch),
     %%ok = application:start(cowboy),
 
-    tacomqtt_sup:start_link(),
-
     {ok, Port} = application:get_env(cowboy_conf, port),
     Dispatch = cowboy_router:compile([{'_', [{"/", request_handler, []}]}]),
-    {ok, _} = cowboy:start_clear(http, 100, [{port, Port}], #{env => #{dispatch => Dispatch}}).
+    {ok, _} = cowboy:start_clear(http, 100, [{port, Port}], #{env => #{dispatch => Dispatch}}),
+
+    tacomqtt_sup:start_link().
 
 %%--------------------------------------------------------------------
 stop(_State) ->
