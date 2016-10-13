b0f1f354b6b7bc17ddf58c4a4092a2ffd881718a
TacoVox <jonas.kahler@icloud.com>
2016-10-13 12:54:10 +0200

    Small update :koala:

--- apps/TacoMQTT/src/tacomqtt_app.erl
+++ apps/TacoMQTT/src/tacomqtt_app.erl
@@ -1,21 +1,31 @@
 %%%-------------------------------------------------------------------
-%% @doc TacoMQTT public API
+%% @doc tacobroker public API
 %% @end
 %%%-------------------------------------------------------------------
 
--module(TacoMQTT_app).
+-module(tacomqtt_app).
 
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
 %%====================================================================
 %% API
 %%====================================================================
 
+start() ->
+    start("", "").
+
 start(_StartType, _StartArgs) ->
-    TacoMQTT_sup:start_link().
+    ok = application:start(cowlib),
+    ok = application:start(ranch),
+    ok = application:start(cowboy),
+
+    tacomqtt_sup:start_link(),
+
+    Dispatch = cowboy_router:compile([{'_', [{"/", request_handler, []}]}]),
+    {ok, _} = cowboy:start_clear(http, 100, [{port, 8080}], #{env => #{dispatch => Dispatch}}).
 
 %%--------------------------------------------------------------------
 stop(_State) ->
